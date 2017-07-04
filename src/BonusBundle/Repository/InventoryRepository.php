3066ac797b00d813f3d9d32f0936f36bfa656561
Matthieu <matthieuy@riseup.net>
2017-07-04 21:01:22 +0200

    Inventory Entity

---
+++ src/BonusBundle/Repository/InventoryRepository.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace BonusBundle\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * Class InventoryRepository
+ * @package BonusBundle\Repository
+ */
+class InventoryRepository extends EntityRepository
+{
+}
