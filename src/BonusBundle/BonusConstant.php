6e56eb9571fb724dc23a780fc778eb1a3a80ed1c
Matthieu <matthieuy@riseup.net>
2017-07-08 20:20:10 +0200

    Limit the inventory size

--- src/BonusBundle/BonusConstant.php
+++ src/BonusBundle/BonusConstant.php
@@ -12,4 +12,9 @@ class BonusConstant
      * Percent of probability to catch a bonus
      */
     const INITIAL_PROBABILITY = 10;
+
+    /**
+     * Inventory size
+     */
+    const INVENTORY_SIZE = 9;
 }
