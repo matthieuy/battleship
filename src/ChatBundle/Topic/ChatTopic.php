5846f58abdd6bfbada7f6260a024a709c76201ae
Matthieu <matthieuy@riseup.net>
2017-07-26 17:01:21 +0200

    Chat topic

---
+++ src/ChatBundle/Topic/ChatTopic.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace ChatBundle\Topic;
+
+use Doctrine\ORM\EntityManager;
+use Gos\Bundle\WebSocketBundle\Client\ClientManipulatorInterface;
+use Gos\Bundle\WebSocketBundle\Router\WampRequest;
+use Gos\Bundle\WebSocketBundle\Topic\PushableTopicInterface;
+use Gos\Bundle\WebSocketBundle\Topic\TopicInterface;
+use Ratchet\ConnectionInterface;
+use Ratchet\Wamp\Topic;
+
+/**
+ * Class ChatTopic
+ * @package ChatBundle\Topic
+ */
+class ChatTopic implements TopicInterface, PushableTopicInterface
+{
+    private $clientManipulator;
+    private $entityManager;
+
+    /**
+     * ChatTopic constructor.
+     * @param ClientManipulatorInterface $clientManipulator
+     * @param EntityManager              $entityManager
+     */
+    public function __construct(ClientManipulatorInterface $clientManipulator, EntityManager $entityManager)
+    {
+        $this->clientManipulator = $clientManipulator;
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * On push
+     * @param Topic        $topic
+     * @param WampRequest  $request
+     * @param string|array $data
+     * @param string       $provider
+     */
+    public function onPush(Topic $topic, WampRequest $request, $data, $provider)
+    {
+    }
+
+    /**
+     * On subscribe
+     * @param ConnectionInterface $connection
+     * @param Topic               $topic
+     * @param WampRequest         $request
+     */
+    public function onSubscribe(ConnectionInterface $connection, Topic $topic, WampRequest $request)
+    {
+    }
+
+    /**
+     * On unsubscribe
+     * @param ConnectionInterface $connection
+     * @param Topic               $topic
+     * @param WampRequest         $request
+     */
+    public function onUnSubscribe(ConnectionInterface $connection, Topic $topic, WampRequest $request)
+    {
+    }
+
+    /**
+     * On publish
+     * @param ConnectionInterface $connection
+     * @param Topic               $topic
+     * @param WampRequest         $request
+     * @param string              $event
+     * @param array               $exclude
+     * @param array               $eligible
+     */
+    public function onPublish(ConnectionInterface $connection, Topic $topic, WampRequest $request, $event, array $exclude, array $eligible)
+    {
+    }
+
+    /**
+     * Get topic name
+     * @return string
+     */
+    public function getName()
+    {
+        return 'chat';
+    }
+}
