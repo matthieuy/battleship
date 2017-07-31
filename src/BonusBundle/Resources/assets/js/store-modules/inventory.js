37b61dd32328b3b11c30156f1ece1db9d226554d
Matthieu <matthieuy@riseup.net>
2017-07-31 22:26:11 +0200

    Bugfix : Bonus counter when == 0

--- src/BonusBundle/Resources/assets/js/store-modules/inventory.js
+++ src/BonusBundle/Resources/assets/js/store-modules/inventory.js
@@ -55,7 +55,7 @@ export default {
         // After each rocket
         [ACTION.AFTER_ROCKET](context, box) {
             // Update inventory
-            if (box.bonus && box.bonus[context.rootState.me.position]) {
+            if (typeof box.bonus !== 'undefined' && typeof box.bonus[context.rootState.me.position] !== 'undefined') {
                 context.commit(MUTATION.INVENTORY.SET_NB, box.bonus[context.rootState.me.position])
                 delete box.life
             }
