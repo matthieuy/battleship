b78da63df9ca7d95e30d576326450ca90b90dd2c
Matthieu <matthieuy@riseup.net>
2017-08-11 13:36:23 +0200

    Create StatsBundle

---
+++ src/StatsBundle/StatsBundle.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace StatsBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class StatsBundle
+ * @package StatsBundle
+ */
+class StatsBundle extends Bundle
+{
+}
