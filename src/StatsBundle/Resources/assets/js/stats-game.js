f91ea6a0ff86d670db4c47451301df24089ac0fc
Matthieu <matthieuy@riseup.net>
2017-11-13 09:50:34 +0100

    Bonus graph

--- src/StatsBundle/Resources/assets/js/stats-game.js
+++ src/StatsBundle/Resources/assets/js/stats-game.js
@@ -1,5 +1,5 @@
 let Highcharts = require('@npm/highcharts/highcharts')
-/* global $, Translator, penaltyData, weaponData */
+/* global $, Translator, penaltyData, weaponData, bonusData */
 
 $(function () {
   // Penalty chart
@@ -83,5 +83,32 @@ $(function () {
       enabled: false,
     },
     series: JSON.parse(weaponData.replace(/&quot;/g, '"')).series,
+  })
+
+  // Bonus
+  Highcharts.chart('bonus', {
+    title: {
+      text: Translator.trans('bonus_name'),
+    },
+    xAxis: {
+      categories: JSON.parse(bonusData.replace(/&quot;/g, '"')).categories,
+      labels: {
+        formatter: function () {
+          return Translator.trans('bonus.' + this.value)
+        },
+      },
+    },
+    tooltip: {
+      formatter: function () {
+        return '<span style="font-size: 10px">' + Translator.trans(this.key) + '</span><br/><span style="color:' + this.color + '">\u25CF</span> ' + this.series.name + ': <b>' + this.y + '</b><br/>'
+      },
+    },
+    yAxis: {
+      allowDecimals: false,
+    },
+    credits: {
+      enabled: false,
+    },
+    series: JSON.parse(bonusData.replace(/&quot;/g, '"')).series,
   })
 })
